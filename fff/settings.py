40049041576e4e73f9b3afbbf046edc72354ffc0
sfsurfer <danlane.intl@gmail.com>
2018-01-08 20:03:16 -0800

    Fix static files

--- fff/settings.py
+++ fff/settings.py
@@ -129,9 +129,9 @@ import os
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+#    os.path.join(BASE_DIR, 'static'),
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
     chartkick.js(),
 )
 
